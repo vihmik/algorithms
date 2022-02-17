3121beda2736d5fa81b06d43bb7ab0aa0c855b20
vihmik <73137058+vihmik@users.noreply.github.com>
2022-02-17 17:19:45 +0200

    Create match.py

---
+++ match.py
@@ -0,0 +1,11 @@
+def match(n, v):
+    
+    res = []
+    for i in range(len(n)):
+        if n[i] == v:
+            res.append(i)
+        else:
+            i = i + 1
+    if res == []:
+        return "Nil"
+    else: return res
